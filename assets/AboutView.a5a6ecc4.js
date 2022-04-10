d7deb6a75df6fa64823d0f0f28c1c7d4da378456
T-Hash06 <tomasfelipe.p@gmail.com>
2022-04-10 14:27:03 -0500

    New Deployment from script

---
+++ assets/AboutView.a5a6ecc4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.2cece76d.js";import{o as t,c as o,b as _}from"./vendor.369cb7d9.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
